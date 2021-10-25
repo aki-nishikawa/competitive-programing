3f6d5bbdb4c1d0284204066be6a89cad6830c7a4
Akihito Nishikawa <akihito.n.0910@keio.jp>
2021-10-25 14:01:49 +0900

    PG battle 2021 mashumaro

---
+++ competition/PGBattle2021-mashumaro/a.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+typedef long long ll;
+int main(){
+    double x, y, k;    cin >> x >> y >> k;
+     cout << fixed << setprecision(15) << y - k/x << endl;
+    return 0;
+}
